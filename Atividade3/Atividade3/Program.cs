3d939064be9e2a6f4f08c383cc4b5e90730812cd
Daniel Filipe <72364158+Daniel10013@users.noreply.github.com>
2023-03-13 20:14:33 -0300

    Add files via upload

---
+++ Atividade3/Atividade3/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vendedor vendedor = new Vendedor();
+            Console.WriteLine("Digite seu nome:");
+            string nome = Console.ReadLine();
+            if (nome == "") {
+                Console.WriteLine("Digite o nome corretamente!");
+                return;
+            }
+            vendedor.setNome(nome);
+            Console.WriteLine("Digite o valor do salário:");
+            double salario = double.Parse(Console.ReadLine());
+            vendedor.setSalario(salario);
+            
+            Console.WriteLine("Digite o valor de vendas:");
+            double valorVendas = double.Parse(Console.ReadLine());
+            vendedor.setQuantidadeVendas(valorVendas);
+
+            double total = vendedor.getValorSalarioTotal();
+            Console.WriteLine(total);
+        }
+    }
+}
---
+++ Atividade3/Atividade3/Vendedor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade3
+{
+    internal class Vendedor
+    {
+        private string nome;
+        private double salario;
+        private double quantidade_vendas;
+
+        public Vendedor() { }
+
+        public Vendedor(string nome, double salario, double quantidade_vendas)
+        {
+            this.nome = nome;
+            this.salario = salario;
+            this.quantidade_vendas = quantidade_vendas;
+        }
+
+        public void setNome(string nome)
+        {
+            this.nome = nome;
+        }
+
+        public string getNome()
+        {
+            return this.nome;
+        }
+
+        public void setSalario(double salario)
+        {
+            this.salario = salario;
+        }
+
+        public double getSalario()
+        {
+            return this.salario;
+        }
+
+        public void setQuantidadeVendas(double quantidade_vendas)
+        {
+            this.quantidade_vendas = quantidade_vendas;
+        }
+
+        public double getQuantidadeVendas()
+        {
+            return this.quantidade_vendas;
+        }
+
+        public double getValorSalarioTotal()
+        {
+            double total = this.salario + (this.quantidade_vendas * 0.15);
+            return total;
+        }
+    }
+}
