cd9c1f87904d9b9937258d2899d2b7a0f6504384
Daniel Filipe <72364158+Daniel10013@users.noreply.github.com>
2023-03-14 20:05:08 -0300

    Add files via upload

---
+++ Atividade4/Atividade4/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo t = new Triangulo();
+
+            Console.WriteLine("Digite o valor dos lados A, B, C do triangulo!");
+            Console.WriteLine("Digite o valor do primeiro lado:");
+            double a_value = double.Parse(Console.ReadLine());
+
+            while(!(a_value > 0))
+            {
+                Console.WriteLine("Digite corretamente o valor para A!");
+                a_value = double.Parse(Console.ReadLine());
+            }
+
+            t.A = a_value;
+
+            Console.WriteLine("Digite o valor do segundo lado:");
+            double b_value = double.Parse(Console.ReadLine());
+
+            while (!(b_value > 0))
+            {
+                Console.WriteLine("Digite corretamente o valor para B!");
+                b_value = double.Parse(Console.ReadLine());
+            }
+            t.B = b_value;
+
+            Console.WriteLine("Digite o valor do segundo lado:");
+            double c_value = double.Parse(Console.ReadLine());
+
+            while (!(c_value > 0))
+            {
+                Console.WriteLine("Digite corretamente o valor para C!");
+                c_value = double.Parse(Console.ReadLine());
+            }
+            t.C = c_value;
+
+            Console.WriteLine(t.checkTriangle());
+        }
+    }
+}
---
+++ Atividade4/Atividade4/Triangulo.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Atividade4
+{
+    internal class Triangulo
+    {
+        private double a;
+        private double b;
+        private double c;
+        
+        public Triangulo () { }
+
+        public Triangulo (double lado_a, double lado_b, double lado_c)
+        {
+            this.a = lado_a;
+            this.b = lado_b;
+            this.c = lado_c;
+        }
+
+        public double A { 
+            set { a = value; }
+            get { return a; }
+        }
+
+        public double B { 
+            set { b = value; } 
+            get { return b; } 
+        }
+
+        public double C
+        {
+            set { c = value; }
+            get { return c; }
+        }
+
+        public string checkTriangle()
+        {
+            if (this.a >= (this.b + this.c))
+            {
+                return "Não é um triangulo";
+            }
+
+            return checkTriangleType();
+        }
+
+        private string checkTriangleType()
+        {
+            if (this.a == this.b && this.b == this.c && this.c == this.a)
+            {
+                return "É um triângulo equilátero";
+            }
+            if (Math.Pow(this.a, 2.00) == (Math.Pow(this.b, 2.00) + Math.Pow(this.c, 2.00)))
+            {
+                return "É um triângulo retangulo";
+            }
+            if (Math.Pow(this.a, 2.00) > (Math.Pow(this.b, 2.00) + Math.Pow(this.c, 2.00)))
+            {
+                return "É um triângulo obtusangulo";
+            }
+            if (Math.Pow(this.a, 2.00) < (Math.Pow(this.b, 2.00) + Math.Pow(this.c, 2.00)))
+            {
+                return "É um triângulo acutangulo";
+            }
+            if (this.a == this.b || this.b == this.c || this.c == this.a)
+            {
+                return "É um triângulo isóceles";
+            }   
+
+            return "";
+        }
+    }
+}
